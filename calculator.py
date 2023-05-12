833295648542269d627bba1a7bcc1698aadf46bb
pororoarx <kengarrido.legaspi@gmail.com>
2023-05-12 09:44:22 +0800

    try and catch the exception

--- calculator.py
+++ calculator.py
@@ -102,15 +102,30 @@ def calculator():
                 number_2 = input("Enter your second number: ")
 
                 # try
+                try:
                     # convert both inputs to float if one or both of the inputs made by user is a float
+                    if "." in number_1 or "." in number_2:
+                        number_1 = float(number_1)
+                        number_2 = float(number_2)
+                    
                     # otherwise, convert both inputs made by user to integers
+                    else:
+                        number_1 = int(number_1)
+                        number_2 = int(number_2)
 
                     # calculate the quotient and print the result
+                    result = number_1 / number_2
+                    print("\nThe quotient is:", result)
                     # break out of the loop
+                    break
 
                 # if the user entered a division by zero error, catch the exception and print an error message
+                except ZeroDivisionError:
+                    print("Invalid input, division by zero error.")
 
                 # if the user entered an invalid input, catch the exception made and print an error message
+                except ValueError:
+                    print("Invalid input")
 
                 # perform a while loop to ask the user if they want to divide again
                     # if yes, break out of the inner loop and repeat the division operation
