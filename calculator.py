adf59352fb7b340c2f9be9060211e32368ad39ce
pororoarx <kengarrido.legaspi@gmail.com>
2023-05-12 09:09:09 +0800

    Pseudocode

--- calculator.py
+++ calculator.py
@@ -4,3 +4,87 @@
 # start
 # print a welcome message
 print("Hello! Welcome to Ken's Calculator App")
+
+# print the operations that users can pick from
+
+
+# define a function called calculator
+    # perform a while loop until the user decided to exit the loop
+        # ask user to enter an operation
+
+        # if user chooses addition
+            # ask the user to enter two numbers
+
+            # try
+                # convert both inputs to float if one or both of the inputs made by user is a float
+                # otherwise, convert both inputs made by user to integers
+
+                # calculate the sum and print the result
+
+            # if the user entered an invalid response, catch the exception and print an error message
+
+
+        # if user chooses subtraction
+            # ask the user to enter two numbers
+
+            # try
+                # convert both inputs to float if one or both of the inputs made by user is a float
+                # otherwise, convert both inputs made by user to integers
+
+                # calculate the difference and print the result
+
+            # if the user entered an invalid response, catch the exception and print an error message
+
+
+        # if user chooses multiplication
+            # ask the user to enter two numbers
+
+            # try
+                # convert both inputs to float if one or both of the inputs made by user is a float
+                # otherwise, convert both inputs made by user to integers
+
+                # calculate the product and print the result
+
+            # if the user entered an invalid response, catch the exception and print an error message
+
+
+        # if user chooses division
+            # perform a while loop until a valid solution for division is performed or user chose to exit
+
+                # ask the user to enter two numbers
+
+                # try
+                    # convert both inputs to float if one or both of the inputs made by user is a float
+                    # otherwise, convert both inputs made by user to integers
+
+                    # calculate the quotient and print the result
+                    # break out of the loop
+
+                # if the user entered a division by zero error, catch the exception and print an error message
+
+                # if the user entered an invalid input, catch the exception made and print an error message
+
+                # perform a while loop to ask the user if they want to divide again
+                    # if yes, break out of the inner loop and repeat the division operation
+                    # if no, return from the function and exit
+                    
+                    # if user entered an invalid response, ask the user to enter either 'YES' or 'NO'
+
+        # ask user if they want to solve again
+
+        # if the user answered yes,
+            # call the calculator function to repeat the process
+
+        # if the user answered no,
+            # print thank you and exit the program
+
+        # if user entered an invalid response, print an error message
+
+
+        # break out of the loop
+
+
+# call the function
+
+
+            
